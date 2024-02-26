2a6196e569d6093865120396ffc8fe92e1d1f0d9
Anton Dodonov <you@example.com>
2024-02-26 12:47:18 +0700

    initial commit

--- hummingbot/smart_components/executors/position_executor/data_types.py
+++ hummingbot/smart_components/executors/position_executor/data_types.py
@@ -1,20 +1,22 @@
 from hummingbot.smart_components.executors.position_executor.base_data_types import (
-    CloseType as OriginalCloseType,
-    PositionConfig as OriginalPositionConfig,
-    PositionExecutorStatus as OriginalPositionExecutorStatus,
+    CloseType as BaseCloseType,
+    PositionConfig as BasePositionConfig,
+    PositionExecutorStatus as BasePositionExecutorStatus,
 )
+
+from hummingbot.core.data_type.common import OrderType
 
 class CloseType(PositionCloseType):
     INSUFFICIENT_BALANCE_TAKER = 8
     TAKER = 9
     
-class PositionConfig(OriginalPositionConfig):
+class PositionConfig(BasePositionConfig):
     maker_perpetual_only_close: Optional[bool] = None
     taker_exchange: str
     taker_pair: str
     taker_profitability: Optional[Decimal] = None
-    taker_order_type OrderType = OrderType.MARKET
+    taker_order_type: OrderType = OrderType.MARKET
     taker_perpetual_only_close: Optional[bool] = None
 
-class PositionExecutorStatus(OriginalPositionExecutorStatus):
-    # ACTIVE_TAKER = 5
+class PositionExecutorStatus(BasePositionExecutorStatus):
+    ACTIVE_TAKER = 5 #not use yet
