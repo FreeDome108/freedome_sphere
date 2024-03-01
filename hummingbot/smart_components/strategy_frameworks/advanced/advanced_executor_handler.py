c6e87f649812336ff5a40004efb2187d5aad5276
Anton Dodonov <anton_dodonov@mail.ru>
2024-03-01 23:35:11 +0700

    initial commit

--- hummingbot/smart_components/strategy_frameworks/advanced/advanced_executor_handler.py
+++ hummingbot/smart_components/strategy_frameworks/advanced/advanced_executor_handler.py
@@ -78,44 +78,44 @@ class AdvancedExecutorHandler(MarketMakingExecutorHandler):
         self.taker_prices[trade_type] = prices
 
     async def control_task(self):
-    if self.controller.all_candles_ready:
-        current_metrics = {
-            TradeType.BUY: self.empty_metrics_dict(),
-            TradeType.SELL: self.empty_metrics_dict()}
-        for order_level in self.controller.config.order_levels:
-            current_executor = self.level_executors[order_level.level_id]
-            if current_executor:
-                closed_and_not_in_cooldown = current_executor.is_closed and not self.controller.cooldown_condition(
-                    current_executor, order_level) or current_executor.close_type == CloseType.EXPIRED
-                active_and_early_stop_condition = current_executor.executor_status == PositionExecutorStatus.ACTIVE_POSITION and self.controller.early_stop_condition(
-                    current_executor, order_level)
-                order_placed_and_refresh_condition = current_executor.executor_status == PositionExecutorStatus.NOT_STARTED and self.controller.refresh_order_condition(
-                    current_executor, order_level)
-                if closed_and_not_in_cooldown:
-                    self.store_executor(current_executor, order_level)
-                elif active_and_early_stop_condition or order_placed_and_refresh_condition:
-                    current_executor.early_stop()
-                elif current_executor.executor_status == PositionExecutorStatus.ACTIVE_POSITION:
-                    current_metrics[current_executor.side]["amount"] += current_executor.filled_amount * current_executor.entry_price
-                    current_metrics[current_executor.side]["net_pnl_quote"] += current_executor.net_pnl_quote
-                    current_metrics[current_executor.side]["executors"].append(current_executor)
-            else:
-                position_config = self.controller.get_position_config(self.taker_prices, order_level)
-                if position_config:
-                    self.create_executor(position_config, order_level)
-        if self.global_trailing_stop_config:
-            for side, global_trailing_stop_conf in self.global_trailing_stop_config.items():
-                if current_metrics[side]["amount"] > 0:
-                    current_pnl_pct = current_metrics[side]["net_pnl_quote"] / current_metrics[side]["amount"]
-                    trailing_stop_pnl = self._trailing_stop_pnl_by_side[side]
-                    if not trailing_stop_pnl and current_pnl_pct > global_trailing_stop_conf.activation_price_delta:
-                        self._trailing_stop_pnl_by_side[side] = current_pnl_pct - global_trailing_stop_conf.trailing_delta
-                        self.logger().info("Global Trailing Stop Activated!")
-                    if trailing_stop_pnl:
-                        if current_pnl_pct < trailing_stop_pnl:
-                            self.logger().info("Global Trailing Stop Triggered!")
-                            for executor in current_metrics[side]["executors"]:
-                                executor.early_stop()
-                            self._trailing_stop_pnl_by_side[side] = None
-                        elif current_pnl_pct - global_trailing_stop_conf.trailing_delta > trailing_stop_pnl:
+        if self.controller.all_candles_ready:
+            current_metrics = {
+                TradeType.BUY: self.empty_metrics_dict(),
+                TradeType.SELL: self.empty_metrics_dict()}
+            for order_level in self.controller.config.order_levels:
+                current_executor = self.level_executors[order_level.level_id]
+                if current_executor:
+                    closed_and_not_in_cooldown = current_executor.is_closed and not self.controller.cooldown_condition(
+                        current_executor, order_level) or current_executor.close_type == CloseType.EXPIRED
+                    active_and_early_stop_condition = current_executor.executor_status == PositionExecutorStatus.ACTIVE_POSITION and self.controller.early_stop_condition(
+                        current_executor, order_level)
+                    order_placed_and_refresh_condition = current_executor.executor_status == PositionExecutorStatus.NOT_STARTED and self.controller.refresh_order_condition(
+                        current_executor, order_level)
+                    if closed_and_not_in_cooldown:
+                        self.store_executor(current_executor, order_level)
+                    elif active_and_early_stop_condition or order_placed_and_refresh_condition:
+                        current_executor.early_stop()
+                    elif current_executor.executor_status == PositionExecutorStatus.ACTIVE_POSITION:
+                        current_metrics[current_executor.side]["amount"] += current_executor.filled_amount * current_executor.entry_price
+                        current_metrics[current_executor.side]["net_pnl_quote"] += current_executor.net_pnl_quote
+                        current_metrics[current_executor.side]["executors"].append(current_executor)
+                else:
+                    position_config = self.controller.get_position_config(self.taker_prices, order_level)
+                    if position_config:
+                        self.create_executor(position_config, order_level)
+            if self.global_trailing_stop_config:
+                for side, global_trailing_stop_conf in self.global_trailing_stop_config.items():
+                    if current_metrics[side]["amount"] > 0:
+                        current_pnl_pct = current_metrics[side]["net_pnl_quote"] / current_metrics[side]["amount"]
+                        trailing_stop_pnl = self._trailing_stop_pnl_by_side[side]
+                        if not trailing_stop_pnl and current_pnl_pct > global_trailing_stop_conf.activation_price_delta:
                             self._trailing_stop_pnl_by_side[side] = current_pnl_pct - global_trailing_stop_conf.trailing_delta
+                            self.logger().info("Global Trailing Stop Activated!")
+                        if trailing_stop_pnl:
+                            if current_pnl_pct < trailing_stop_pnl:
+                                self.logger().info("Global Trailing Stop Triggered!")
+                                for executor in current_metrics[side]["executors"]:
+                                    executor.early_stop()
+                                self._trailing_stop_pnl_by_side[side] = None
+                            elif current_pnl_pct - global_trailing_stop_conf.trailing_delta > trailing_stop_pnl:
+                                self._trailing_stop_pnl_by_side[side] = current_pnl_pct - global_trailing_stop_conf.trailing_delta
